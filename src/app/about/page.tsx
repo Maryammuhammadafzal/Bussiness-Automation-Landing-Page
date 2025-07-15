91f8f70c67c007d3c8733d70bfeea8ca6bb7723f
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-15 16:09:31 +0500

    created about section

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div className='w-full h-auto flex justify-center items-center md:py-20 py-10'>
+            <div className='w-full flex px-3 justify-center items-center text-center flex-col gap-3 h-auto'>
+                <h2 className='font-semibold lg:text-5xl sm:text-4xl xs:text-3xl text-2xl'>Simplify. Streamline. Succeed.</h2>
+                <p className='font-light sm:text-base xs:text-sm text-xs max-w-4xl'>Unlock the full potential of your business with intelligent automation. At [Your Business Automation Company Name], we help organizations automate repetitive tasks, optimize workflows, and enhance decision-making with cutting-edge technology solutions.</p>
+            </div>
+        </div>
+    )
+}
+
+export default AboutPage
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
 'use client'
 import { MenuIcon, X } from 'lucide-react'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [offset, setOffset] = useState(0);
+    useEffect(() => {
+        const handleScroll = () => {
+            setOffset(window.scrollY);
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    } , []);
     const nav_data = [
         {
             name: 'Home',
@@ -26,9 +34,11 @@ const Header = () => {
             name: 'Contact Us',
             link: '/'
         },
-    ]
+    ];
+    console.log(offset);
+    
     return (
-        <div className='w-full h-[120px] flex justify-center items-center fixed top-0 left-0'>
+        <div className={`${offset > 0 ? 'bg-black' : 'bg-transparent' } w-full h-[100px] flex justify-center items-center fixed top-0 left-0`}>
             <div className='w-full h-full flex justify-between items-center gap-4 lg:px-8 px-5'>
                 <Link href='/' className='w-auto h-auto'>
                     <p className='logo text-[40px] font-semibold px-2'>LOGO</p>
@@ -47,7 +57,7 @@ const Header = () => {
                     <nav className={`${isOpen ? 'translate-x-0 w-50 flex' : 'translate-x-50 w-0 hidden'} sm:hidden h-screen bg-black duration-700 fixed right-0 top-0`}>
                         <ul className='w-full h-full py-6 px-3 flex flex-col gap-6'>
                             <li className='w-full flex justify-end items-center p-3'>
-                                <X onClick={()=> setIsOpen(false)} size={32} />
+                                <X onClick={() => setIsOpen(false)} size={32} />
                             </li>
                             {
                                 nav_data.map(({ name, link }, index) => (
