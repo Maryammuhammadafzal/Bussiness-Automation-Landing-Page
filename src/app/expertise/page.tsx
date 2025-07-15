593e93c29e25d8b3c6655ec516c72d5205caa635
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-15 16:55:43 +0500

    created responsive

--- src/app/expertise/page.tsx
+++ src/app/expertise/page.tsx
@@ -3,35 +3,35 @@ import React from 'react'
 
 const ExpertisePage = () => {
     return (
-        <div className='w-full h-auto flex md:py-20 py-10 flex-col gap-4 px-8'>
+        <div className='w-full h-auto flex md:py-20 py-10 flex-col gap-4 sm:px-8 px-4'>
             <Heading title='Our Expertise' />
-            <div className='flex flex-row w-full h-auto'>
-                <div className='w-1/2 h-auto border-r border-secondary'>
-                    <div className='w-full border-b h-auto flex flex-col gap-3 border-secondary py-10'>
-                        <h3 className='gradient-title font-semibold xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl text-xl'>Workflow Automation</h3>
+            <div className='flex xs:flex-row flex-col w-full h-auto'>
+                <div className='xs:w-1/2 w-full h-auto xs:border-r border-secondary'>
+                    <div className='w-full border-b sm:h-[230px] xs:h-[200px] h-auto flex flex-col gap-3 border-secondary py-10 px-1'>
+                        <h3 className='gradient-title font-semibold max-md:leading-6 xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl text-lg'>Workflow Automation</h3>
                         <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Say goodbye to manual processes. We design and implement end-to-end workflow automation solutions to help your teams focus on what matters most—growth and innovation.</p>
                     </div>
-                    <div className='w-full border-b h-auto flex flex-col gap-3 border-secondary py-10'>
-                        <h3 className='gradient-title font-semibold xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl text-xl'>Workflow Automation</h3>
-                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Say goodbye to manual processes. We design and implement end-to-end workflow automation solutions to help your teams focus on what matters most—growth and innovation.</p>
+                    <div className='w-full border-b sm:h-[230px] xs:h-[200px] h-auto flex flex-col gap-3 border-secondary py-10 px-1'>
+                        <h3 className='gradient-title font-semibold max-md:leading-6 xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl text-lg'>CRM & Sales Automation</h3>
+                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Boost your customer relationships and sales performance with smart CRM tools. Automate follow-ups, manage pipelines, and personalize communication effortlessly.</p>
                     </div>
-                    <div className='w-full h-auto flex flex-col gap-3 py-10'>
-                        <h3 className='gradient-title font-semibold xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl text-xl'>Workflow Automation</h3>
-                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Say goodbye to manual processes. We design and implement end-to-end workflow automation solutions to help your teams focus on what matters most—growth and innovation.</p>
+                    <div className='w-full sm:h-[230px] xs:h-[200px] h-auto  max-xs:border-b border-secondary flex flex-col gap-3 py-10 px-1'>
+                        <h3 className='gradient-title font-semibold max-md:leading-6 xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl text-lg'>Marketing Automation</h3>
+                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Streamline your campaigns, capture leads, and increase ROI with automation tools that manage emails, social media, and analytics all in one place.</p>
                     </div>
                 </div>
-                <div className='w-1/2 h-auto'>
-                    <div className='w-full border-b h-auto flex flex-col gap-3 border-secondary px-8 py-10'>
-                        <h3 className='gradient-title font-semibold xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl text-xl'>Workflow Automation</h3>
-                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Say goodbye to manual processes. We design and implement end-to-end workflow automation solutions to help your teams focus on what matters most—growth and innovation.</p>
+                <div className='xs:w-1/2 w-full h-auto'>
+                    <div className='w-full border-b sm:h-[230px] xs:h-[200px] h-auto flex flex-col gap-3 border-secondary sm:pl-8 pl-4 py-10 px-1'>
+                        <h3 className='gradient-title font-semibold max-md:leading-6 xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl text-lg'>Finance Automation</h3>
+                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Reduce human error and increase efficiency in invoicing, payroll, and reporting with AI-powered finance automation solutions.</p>
                     </div>
-                    <div className='w-full border-b h-auto flex flex-col gap-3 border-secondary px-8 py-10'>
-                        <h3 className='gradient-title font-semibold xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl text-xl'>Workflow Automation</h3>
-                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Say goodbye to manual processes. We design and implement end-to-end workflow automation solutions to help your teams focus on what matters most—growth and innovation.</p>
+                    <div className='w-full border-b sm:h-[230px] xs:h-[200px] h-auto flex flex-col gap-3 border-secondary sm:pl-8 pl-4 py-10 px-1'>
+                        <h3 className='gradient-title font-semibold max-md:leading-6 xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl text-lg'>HR & Employee Onboarding</h3>
+                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Enhance employee experience from day one. Automate onboarding, documentation, and training with tailored HR automation solutions.</p>
                     </div>
-                    <div className='w-full h-auto flex flex-col gap-3 px-8 py-10'>
-                        <h3 className='gradient-title font-semibold xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl text-xl'>Workflow Automation</h3>
-                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>Say goodbye to manual processes. We design and implement end-to-end workflow automation solutions to help your teams focus on what matters most—growth and innovation.</p>
+                    <div className='w-full sm:h-[230px] xs:h-[200px] h-auto flex flex-col gap-3 sm:pl-8 pl-4 py-10 px-1'>
+                        <h3 className='gradient-title font-semibold max-md:leading-6 xl:text-[40px] lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl text-lg'>Custom Integration</h3>
+                        <p className='font-light md:text-base sm:text-sm text-xs max-w-xl'>We connect your favorite tools and platforms to create seamless automated ecosystems tailored to your unique business needs.</p>
                     </div>
                 </div>
             </div>
