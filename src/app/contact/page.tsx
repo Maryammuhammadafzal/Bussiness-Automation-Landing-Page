1a03a81f6536250b265fd5a612ee31d8b7b3745c
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-16 14:08:46 +0500

    creating responsive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,23 +7,23 @@ import React from "react";
 
 const ContactPage = () => {
     return (
-        <div className="px-8 md:py-20 sm:py-14 py-7 w-full h-auto flex gap-16 flex-col ">
+        <div className="px-8 md:py-20 sm:py-14 py-7 w-full h-auto flex md:gap-16 sm:gap-10 gap-6 flex-col ">
             <Heading title="Let’s Start Planning" />
-            <div className=" h-auto flex flex-col gap-4 w-full ">
-                <h3 className='font-semibold sm:text-2xl text-xl'>
+            <div className=" h-auto flex flex-col gap-3 w-full ">
+                <h3 className='gradient-title w-fit font-semibold xl:text-[40px] lg:text-4xl sm:text-3xl text-2xl max-md:max-w-sm max-sm:max-w-[300px]'>
                     {`Let’s Automate Your Success`}
                 </h3>
-                <p className='sm:text-base text-sm'>
+                <p className='sm:text-base text-sm lg:max-w-2xl md:max-w-xl sm:max-w-sm max-w-[300px] '>
                     Join the future of work. Contact us today to schedule your free consultation and discover how automation can revolutionize your business.
                 </p>
             </div>
-            <div className="max-w-2xl h-auto flex flex-col gap-3 justify-center items-center">
-                <div className="flex flex-wrap gap-8 ">
-                    <div className="grid min-w-xs items-center gap-1.5 ">
+            <div className="max-w-[620px] h-auto flex flex-col gap-3 justify-center items-center">
+                <div className="flex flex-wrap md:gap-8 gap-5">
+                    <div className="grid sm:max-w-[290px] w-full items-center gap-1.5 ">
                         <Label className="text-secondary text-base" htmlFor="name">Name</Label>
                         <Input type="text" id="name" className="h-[40px] border-image" />
                     </div>
-                    <div className="grid min-w-xs items-center gap-1.5 ">
+                    <div className="grid sm:max-w-[290px] w-full items-center gap-1.5 ">
                         <Label className="text-secondary text-base" htmlFor="email">Email</Label>
                         <Input type="email" id="email" className="h-[40px] border-image" />
                     </div>
