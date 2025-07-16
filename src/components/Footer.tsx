da628f22ee45ce0513a5c35eb78ab8dff1aaa357
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-07-16 14:18:20 +0500

    complete responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,85 +18,78 @@ const Footer = () => {
         },
     ];
     return (
-        <div className="w-full lato text-primary h-auto gap-5 flex justify-center items-center p-3">
-            <div className="academySection w-[80%] max-lg:w-[95%] max-md:w-full  h-auto flex flex-col gap-4 justify-center items-center">
-                <div className="footerContent max-sm:justify-center w-full h-auto flex flex-wrap justify-between mb-3 ">
-                    <div className="left w-[25%] max-sm:w-[100%] max-[900px]:w-[45%] max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center p-3 h-auto">
+        <div className="w-full lato text-secondary h-auto gap-5 flex justify-center items-center p-3">
+            <div className=" w-[80%] max-lg:w-[95%] max-md:w-full  h-auto flex flex-col gap-4 justify-center items-center">
+                <div className="footerContent max-sm:justify-center w-full h-auto grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 justify-between mb-3 gap-6 ">
+                    <div className="left w-full max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:items-center p-3 h-auto">
                         {/* Logo */}
                         <div className="logo ">
                             <Link href='/' className='w-auto h-auto'>
                                 <p className='logo text-[40px] font-semibold px-2'>LOGO</p>
                             </Link>
                         </div>
-                        <p className="text-lg  my-5 max-sm:w-[70%] max-sm:text-center font-[Nunito Sans] max-md:text-[14px] max-sm:text-[12px] max-sm:w-full font-[400]">
-                            Let’s make life sweeter together! Reach us at
-                        </p>
                     </div>
 
-                    <div className="middle max-sm:w-full max-sm:py-3 w-[20%] p-3 h-auto ">
+                    <div className="middle max-sm:w-full max-sm:py-3 w-full p-3 h-auto ">
                         <h4 className="text-secondary text-xl  lato capitalize max-lg:text-lg max-md:text-base">
                             Pages
                         </h4>
                         <ul className="list-none text-base py-2  gap-1 flex flex-col ">
-                            <li className=" font-[Nunito Sans]  font-[400]">
+                            <li className="  font-normal">
                                 <a href="#" className="">
                                     Home
                                 </a>
                             </li>
-                            <li className=" font-[Nunito Sans]  font-[400]">
+                            <li className="  font-normal">
                                 <a href="#" className="">
                                     Products
                                 </a>
                             </li>
-                            <li className=" font-[Nunito Sans]  font-[400]">
+                            <li className="  font-normal">
                                 <a href="#" className="">
                                     About Us
                                 </a>
                             </li>
-                            <li className=" font-[Nunito Sans]  font-[400]">
+                            <li className="  font-normal">
                                 <a href="#" className="">
                                     Contact us
                                 </a>
                             </li>
                         </ul>
                     </div>
-                    <div className="middle max-sm:w-auto max-sm:py-3 max-sm:mx-auto w-[25%] p-3 h-auto">
+                    <div className="middle  max-sm:py-3 w-full p-3 h-auto">
                         <h4 className="text-secondary text-xl lato capitalize max-lg:text-lg max-md:text-base">
                             Get In Touch
                         </h4>
                         <ul className="list-none py-2 gap-1 flex flex-col ">
-                            <li className="text-sm font-[Nunito Sans]  font-[400] ">
+                            <li className="text-sm  font-normal ">
                                 4517 Washington Ave. Manchester, Kentucky 39495
                             </li>
-                            <li className="text-sm font-[Nunito Sans]  font-[400]">
+                            <li className="text-sm  font-normal">
                                 jessica.hanson@example.com
                             </li>
-                            <li className="text-sm font-[Nunito Sans]  font-[400]">
+                            <li className="text-sm  font-normal">
                                 (308) 555-0121
                             </li>
                         </ul>
                     </div>
-                    <div className="left max-sm:w-full max-sm:px-3  max-sm:py-3 w-[25%] p-3  h-auto max-[900px]:w-full">
+                    <div className="left max-sm:w-full max-sm:px-3 gap-10  max-sm:py-3 w-full p-3  h-auto max-[900px]:w-full">
                         <h4 className="text-secondary text-lg capitalize max-lg:text-base lato max-md:text-sm">
                             Social Media
                         </h4>
-                        <div className="flex gap-3 py-1">
-                            <FaFacebook size={20} />
-                            <FaXTwitter size={20} />
-                            <FaSquareInstagram size={20} />
-                            <FaLinkedin size={20} />
-                            <FaWhatsapp size={20} />
+                        <div className="flex gap-x-10 gap-y-6 flex-wrap h-auto max-w-[200px]  py-1">
+                            <FaFacebook size={30} />
+                            <FaXTwitter size={30} />
+                            <FaSquareInstagram size={30} />
+                            <FaLinkedin size={30} />
+                            <FaWhatsapp size={30} />
 
                         </div>
                     </div>
                 </div>
-                <div className="copyright flex sm:flex-row flex-col gap-4 justify-between items-center w-full h-auto py-5">
-                    <p className="md:text-sm text-xs">© 2021 all copyright reserved. All Rights Reserved.</p>
-                    <ul className="flex gap-2 md:text-sm text-xs">
-                        <li>Term of services</li>
-                        <li>Privacy Policy</li>
-                        <li>Cookie Policy</li>
-                    </ul>
+                <div className="copyright flex gap-4 justify-center items-center w-full h-auto py-5">
+                    <p className="md:text-sm text-xs text-center">© 2021 all copyright reserved. All Rights Reserved. <Link href='/' className="underline"> Rootlet Solution</Link></p>
+
                 </div>
             </div>
         </div>
